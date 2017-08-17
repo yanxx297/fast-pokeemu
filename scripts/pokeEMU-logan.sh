9a04d3ac685f4cd8dce1255dfc51897b80fc5ebc
Qiuchen Yan <yan@logan.cs.umn.edu>
2017-08-17 14:35:55 -0500

    Update the script running performance experiment on logan

--- scripts/pokeEMU-logan.sh
+++ scripts/pokeEMU-logan.sh
@@ -3,31 +3,39 @@ dir=/export/scratch/tmp
 in=$dir/aggreg_list/
 out=$dir/out/
 
+# output performance results
+count_time () {
+        for folder in $dir/aggreg-$1-qemu/*; 
+        do 
+                for dump in $folder/*.post; 
+                do 
+                        for log in $in/$(basename $folder)/*.log; 
+                        do 
+                                if grep -q $(basename $dump .post) $log; then 
+                                        cat $log >> count-$1; 
+                                fi; 
+                        done; 
+                done; 
+        done;
+        for folder in $dir/aggreg-$1-qemu/*; 
+        do 
+                echo $folder; 
+                cat $folder/time >> time-$1; 
+        done
+}
+
 #./run-testcase.sh -aggreg -m 1 -in $in -out $out
 #mv $out $dir/aggreg-m1-qemu/
+#count_time m1
 #./run-testcase.sh -aggreg -m 2 -in $in -out $out
 #mv $out $dir/aggreg-m2-qemu/
+#count_time m2
+#./run-testcase.sh -aggreg -m 3 -in $in -out $out
+#mv $out $dir/aggreg-m3-qemu/
+#count_time m3
 
 # for loop=10000 mode3
-./run-testcase.sh -aggreg -m 3 -in $in -out $out
-mv $out $dir/aggreg-m3-qemu-10000/
+#./run-testcase.sh -aggreg -m 3 -in $in -out $out
+#mv $out $dir/aggreg-10000m3-qemu/
+count_time 10000m3
 
-# output performance results
-# TODO: finish it
-for dir in /home/grad01/yan/tmp/single-m0-qemu/*; 
-do 
-	for dump in $dir/*.post; 
-	do 
-		for log in /home/grad01/yan/tmp/aggreg_list/$(basename $dir)/*.log; 
-		do 
-			if grep -q $(basename $dump .post) $log; then 
-				cat $log >> count-m0; 
-			fi; 
-		done; 
-	done; 
-done;
-for dir in /home/grad01/yan/tmp/single-m0-qemu/*; 
-do 
-	echo $dir; 
-	cat $dir/time >> tmp-m0; 
-done
