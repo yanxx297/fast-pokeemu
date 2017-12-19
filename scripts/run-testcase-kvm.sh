f3d8ffd05e6572f2eb1ac3d569c751a6b7832548
yanxx297 <yanxx297@umn.edu>
2017-12-19 14:38:23 -0600

    Remove unused scripts - Scripts for running experiments in multiple machines - Scripts we no longer need

--- scripts/run-testcase-kvm.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-remote_dir=/export/scratch/tmp/out
-dirnames=$(ssh yan@logan.cs.umn.edu $(echo ls $remote_dir))
-for dir in $dirnames; 
-do
-	echo $remote_dir/$dir
-	tcnames=$(ssh yan@logan.cs.umn.edu $(echo ls $remote_dir/$dir))
-done;
