yanxx297/fast-pokeemu

commit 9da7fb423962066f250dcd2301d28e83fe49f5e3

Adjust organization of project repository - merge emu and emulator - merge tools and lib - add scripts used in VEE18 - modify gitignore

scripts/bisect.sh
@@ -1,0 +1,11 @@
+#! /bin/bash
+python run_test_case.py testcase:/home/yanxx297/Project/pokemu-oras/data/state-explr/ARPL_EwGw/00000000/testcase timeout:10 outdir:/tmp/out script:/home/yanxx297/Project/pokemu-oras/emu/qemu-0.12.4/run-testcase-debug mode:2
+../emu/qemu-0.12.4/run-testcase /tmp/floppy-dbg /tmp/out/dbg.post /tmp/out
+../emulators/kvm-run/run-testcase /tmp/out/dbg.post.pre /tmp/out/00000000-kvm.post
+python diff_cpustate.py /tmp/out/dbg.post.post /tmp/out/00000000-kvm.post
+rv=$?
+if [ $rv -ne 0 ]; then
+        exit 0
+else
+        exit 1
+fi

emu/bisect.sh
@@ -1,0 +1,7 @@
+#! /bin/bash
+git bisect start HEAD afc6ffd005af42b7f411e660f493ef91fcaefa39
+git bisect run ./checkfix-rev.sh
+git bisect log > $1
+git checkout .
+git bisect reset
+

commit a7344be078ddc756ce2108004d24f692d4a37e79

Fix scripts broken by 9da7fb4 and f3d8ffd - Add split_log.py back - Rename /emulators with /emu - Add options to pokeEMU-offline.sh

scripts/batchRunTestcase-kvm-offline
@@ -3,7 +3,7 @@ SHELL := /bin/bash
 
 # Config
 OUT?=/tmp/out/
-EMUPATH?=./../emulators/kvm-run/run-testcase
+EMUPATH?=./../emu/kvm-run/run-testcase
 INSN_LIST?=
 TIMEOUT?=10
 

scripts/bisect.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 python run_test_case.py testcase:/home/yanxx297/Project/pokemu-oras/data/state-explr/ARPL_EwGw/00000000/testcase timeout:10 outdir:/tmp/out script:/home/yanxx297/Project/pokemu-oras/emu/qemu-0.12.4/run-testcase-debug mode:2
 ../emu/qemu-0.12.4/run-testcase /tmp/floppy-dbg /tmp/out/dbg.post /tmp/out
-../emulators/kvm-run/run-testcase /tmp/out/dbg.post.pre /tmp/out/00000000-kvm.post
+../emu/kvm-run/run-testcase /tmp/out/dbg.post.pre /tmp/out/00000000-kvm.post
 python diff_cpustate.py /tmp/out/dbg.post.post /tmp/out/00000000-kvm.post
 rv=$?
 if [ $rv -ne 0 ]; then

scripts/pokeEMU-offline.sh
@@ -4,6 +4,8 @@ in=/home/yanxx297/Project/pokemu-oras/data/state-explr
 out=$dir/out/
 q_out=$out/qemu/
 k_out=$out/kvm/
+start=10
+single=false
 
 is_match () {
 	if [ -e $1/mismatch ]; then
@@ -15,15 +17,43 @@ is_match () {
 	fi
 }
 
-echo 'Run single mode 3 experiment'
-if ! [ -e $dir/single-m3-kvm/ ]; then
+while [ "$1" != "" ]; do
+        case $1 in
+                -n )
+                        single=true
+                        ;;
+                -s | --start )
+                        shift
+                        start=$1
+                        ;;                    
+                -h | --help )
+                        echo "Usage: ./pokeEMU-offline.sh [-s STARTING_POINT] [-n]
+                        0       Run full test (single mode 3)
+                        1       Generate aggregating list
+                        2       Single mode 0
+                        3       Aggregated mode 3
+                        4       generate final results"
+                        ;;
+        esac
+        shift
+done
+
+if (( $start >= 10 )); then
+        exit 0
+fi
+
+if (( $start <= 0 )); then
+        echo 'Run single mode 3 experiment'
 	./run-testcase-offline.sh -m 3 -in $in -out $out
 	./run-testcase-offline.sh -kvm -in $in -out $out
 	mv $out $dir/single-m3-kvm/
+        if [ "$single" == true ]; then
+                exit 0
+        fi
 fi
 
-# Generate the list of valid test cases for aggregating
-if ! [ -e aggreg_list ] && ! [ -e $dir/aggreg-m3-kvm/ ]; then
+if (( $start <= 1 )); then
+        echo 'Generate the list of valid test cases for aggregating'
 	mkdir -p aggreg_list
 	for insn in $dir/single-m3-kvm/*; do
 		mkdir aggreg_list/$(basename $insn)
@@ -36,17 +66,23 @@ if ! [ -e aggreg_list ] && ! [ -e $dir/aggreg-m3-kvm/ ]; then
 		rm aggreg_list/$(basename $insn)/log
 	done
         mv aggreg_list/ $dir/aggreg_list/
+        if [ "$single" == true ]; then
+                exit 0
+        fi
 fi
 
-echo 'Run single mode 0 experiment'
-if ! [ -e $dir/single-m0-kvm/ ]; then
+if (( $start <= 2 )); then
+        echo 'Run single mode 0 experiment'
         ./run-testcase-offline.sh -m 0 -in $in -out $out
 	./run-testcase-offline.sh -kvm -in $in -out $out
 	mv $out $dir/single-m0-kvm/
+        if [ "$single" == true ]; then
+                exit 0
+        fi
 fi
 
-echo 'Run aggregate mode 3 experiment'
-if ! [ -e $dir/aggreg-m3-kvm/ ]; then
+if (( $start <= 3 )); then
+        echo 'Run aggregate mode 3 experiment'
 	./run-testcase-offline.sh -aggreg -m 3 -in $dir/aggreg_list/ -out $out
         # Compute mode 3 performance results
         for folder in $out*;
@@ -64,10 +100,13 @@ if ! [ -e $dir/aggreg-m3-kvm/ ]; then
         done;
 	./run-testcase-offline.sh -kvm -in $in -out $out
 	mv $out $dir/aggreg-m3-kvm/
+        if [ "$single" == true ]; then
+                exit 0
+        fi
 fi
 
-echo 'generate effectiveness results'
-if ! [ -e $out/000 ]; then
+if (( $start <= 4 )); then
+        echo 'generate effectiveness results'
 	mkdir -p $out
 	touch $out/000 $out/001 $out/010 $out/011 $out/100 $out/101 $out/110 $out/111
 	for aggreg in $dir/aggreg-m3-kvm/*; do

scripts/split_log.py
@@ -1,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import sys
+
+def split_file(file, splitlen):
+    if not os.path.isfile(file):
+        return
+    input = open(file, 'r').read().split(',')
+    idx = 0
+    for val in range(0, len(input), splitlen):
+        data = input[val:val+splitlen]
+        name = os.path.dirname(file) + "/%d.log" % idx
+        print name
+        output = open(name, 'w')
+        output.write(','.join(data))
+        output.close()
+        idx += 1
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    splitlen = sys.argv[2]
+    split_file(filename, int(splitlen))